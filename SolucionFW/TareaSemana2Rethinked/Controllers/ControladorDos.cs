2c2b42bbe45890fe148879add33e58d28dc64ad4
Alejandro Ruiz <joseruizsuarez@hotmail.com>
2022-02-16 14:44:50 -0300

    Clase SEMANA 3
    Trabajo en clase de la semana 3 y correcciones de la Tarea 1

---
+++ SolucionFW/TareaSemana2Rethinked/Controllers/ControladorDos.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TareaSemana2Rethinked.Controllers
+{
+    public class ControladorDos : Controller
+    {
+        public IActionResult VistaTres()
+        {
+            return View();
+        }
+
+        public IActionResult VistaCuatro()
+        {
+            return View();
+        }
+    }
+}
---
+++ SolucionFW/TareaSemana2Rethinked/Controllers/ControladorUno.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TareaSemana2Rethinked.Controllers
+{
+    public class ControladorUno : Controller
+    {
+        public IActionResult VistaUno()
+        {
+            return View();
+        }
+
+        public IActionResult VistaDos()
+        {
+            return View();
+        }
+    }
+}
